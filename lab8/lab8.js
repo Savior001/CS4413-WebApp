99b0da2c375c8ff80eff65200fb03862f9a458b9
Savior001 <45613403+Savior001@users.noreply.github.com>
2023-10-22 15:52:30 -0500

    Updating lab8.js to use proper input ids

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,18 +1,19 @@
 function getDataFromForm() {
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
+    const firstName = document.querySelector('input[name="fname"]').value;
+    const lastName = document.querySelector('input[name="lname"]').value;
     runAjax(firstName, lastName);
 }
 
 function runAjax(fname, lname) {
     const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "./ajax.php?fname=" + fname + "&lname=" + lname, true);
-
-    xhr.onload = function () {
+    xhr.open("GET", `./ajax.php?fname=${fname}&lname=${lname}`, true);
+    xhr.onload = function() {
         if (xhr.status === 200) {
-            if (typeof xhr.responseText === 'string') {
-                document.getElementById('responseString').textContent = xhr.responseText;
+            const responseText = xhr.responseText;
+            if (typeof responseText === 'string') {
+                const responseParagraph = document.getElementById("responseString");
+                responseParagraph.textContent = responseText;
             }
         }
     };
