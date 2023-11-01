4d8dec9af5b332ce947bea7114447d50aa451af7
Savior001 <45613403+Savior001@users.noreply.github.com>
2023-11-01 17:20:51 -0500

    adding lab10

---
+++ lab10/show_data.php
@@ -0,0 +1,17 @@
+<?php
+
+$data_file = 'datafiles/data.txt';
+
+
+$data_array = file($data_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+echo '<html><head><title>Data Viewer</title></head><body>';
+echo '<h1>Data Viewer</h1>';
+echo '<table border="1"><tr><th>First Name</th><th>Last Name</th></tr>';
+foreach ($data_array as $line) {
+    list($first_name, $last_name) = explode(' ', $line, 2);
+    echo '<tr><td>' . htmlspecialchars($first_name) . '</td><td>' . htmlspecialchars($last_name) . '</td></tr>';
+}
+echo '</table>';
+echo '</body></html>';
+?>
---
+++ save_data.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+
+    
+    $data = $first_name . ' ' . $last_name . PHP_EOL;
+
+    
+    $data_file = 'datafiles/data.txt';
+
+    file_put_contents($data_file, $data, FILE_APPEND);
+
+    header('Location: lab10/index.html');
+    exit;
+}
+?>
