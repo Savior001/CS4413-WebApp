ebaf8fc3b06f6ed6fe1fc19aa298f1ccd185e999
Savior001 <45613403+Savior001@users.noreply.github.com>
2023-11-05 20:01:22 -0600

    updating save_data.php

--- save_data.php
+++ save_data.php
@@ -1,8 +1,29 @@
 <?php
 require_once 'vendor/autoload.php';
 
-use Azure\Storage\Blob\BlobServiceClient;
-use Azure\Storage\Blob\BlobRestProxy;
+use Azure\Core\AzureCliCredential;
+use Azure\Storage\Blobs\BlobServiceClient;
+use Azure\Storage\Blobs\Models\CreateContainerOptions;
+use Azure\Storage\Blobs\Models\BlobStreamOptions;
+
+static function UploadBlob(string $accountName, string $containerName, string $blobName, string $blobContents): void {
+    $blobServiceClient = new BlobServiceClient('https://' . $accountName . '.blob.core.windows.net', new AzureCliCredential());
+
+    $containerClient = $blobServiceClient->getContainerClient($containerName);
+    if (!$containerClient->exists()) {
+        $containerClient->create(new CreateContainerOptions());
+    }
+
+    $blobClient = $containerClient->getBlobClient($blobName);
+    $blobStreamOptions = new BlobStreamOptions();
+    $blobStreamOptions->setAccessTier('Hot'); 
+    $blobStreamOptions->setContentType('text/plain');
+    $blobStreamOptions->setContentLanguage('en-US');
+    $blobStreamOptions->setContentDisposition('attachment');
+    $blobStreamOptions->setContentEncoding('gzip');
+    $blobStreamOptions->setContentMD5(base64_encode(md5($blobContents, true)));
+    $blobClient->uploadData($blobContents, $blobStreamOptions);
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $first_name = $_POST['first_name'];
@@ -12,29 +33,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data_file = 'datafiles/data.txt';
 
     file_put_contents($data_file, $data, FILE_APPEND);
-    
-    $connectionString = "DefaultEndpointsProtocol=https;AccountName=cs4413webappstorage;AccountKey=b8TgJ53iXdv4KTrocr/eZSXTqygxgqr7+UuXFrLuSmTYXZTRmooZg1AN4WM99yy4AvvClXBLGu6g+ASt2gxAAg==;";
-    $containerName = 'cs4413-blob';
-
-    $blobServiceClient = BlobServiceClient::createConnection($connectionString);
-    $containerClient = $blobServiceClient->getContainerClient($containerName);
-
-    if (file_exists($data_file)) {
-        $fileContent = file_get_contents($data_file);
-        $blobName = 'data.txt';
-        $blobClient = $containerClient->getBlobClient($blobName);
-
-        try {
-            $blobClient->upload($fileContent);
-            echo "Data has been successfully uploaded to Azure Blob Storage.";
-        } catch (Exception $e) {
-            echo "Error: Unable to upload data to Azure Blob Storage.";
-        }
-    } else {
-        echo "Error: Local file not found.";
-    }
-
-    header('Location: lab10/index.html');
-    exit;
+    UploadBlob('cs4413webappstorage', 'cs4413-blob', 'data.txt', $data);
 }
 ?>
