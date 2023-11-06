144482b44b0733a77597bd2922aa38798159da59
Savior001 <45613403+Savior001@users.noreply.github.com>
2023-11-05 18:55:40 -0600

    updating save_data.php

--- save_data.php
+++ save_data.php
@@ -1,19 +1,5 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-
-    $data = $first_name . ' ' . $last_name . PHP_EOL;
-    $data_file = 'datafiles/data.txt';
-    file_put_contents($data_file, $data, FILE_APPEND)
-
-    header('Location: lab10/index.html');
-    exit;
-}
-?>
-
-<?php
-require_once 'vendor/autoload.php'; // Include Azure SDK
+require_once 'vendor/autoload.php';
 
 use Azure\Storage\Blob\BlobServiceClient;
 use Azure\Storage\Blob\BlobRestProxy;
@@ -25,24 +11,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = $first_name . ' ' . $last_name . PHP_EOL;
     $data_file = 'datafiles/data.txt';
 
-    // Append the data to the local file
     file_put_contents($data_file, $data, FILE_APPEND);
     
-    // Azure Storage Account credentials and container name
-    $connectionString = "DefaultEndpointsProtocol=https;AccountName=cs4413webappstorage;AccountKey=b8TgJ53iXdv4KTrocr/eZSXTqygxgqr7+UuXFrLuSmTYXZTRmooZg1AN4WM99yy4AvvClXBLGu6g+ASt2gxAAg==;EndpointSuffix=core.windows.net";
+    $connectionString = "DefaultEndpointsProtocol=https;AccountName=cs4413webappstorage;AccountKey=b8TgJ53iXdv4KTrocr/eZSXTqygxgqr7+UuXFrLuSmTYXZTRmooZg1AN4WM99yy4AvvClXBLGu6g+ASt2gxAAg==;";
     $containerName = 'cs4413-blob';
 
-    // Create a BlobServiceClient instance
     $blobServiceClient = BlobServiceClient::createConnection($connectionString);
-
-    // Get the Blob Container client
     $containerClient = $blobServiceClient->getContainerClient($containerName);
 
-    // Check if the file exists locally
     if (file_exists($data_file)) {
         $fileContent = file_get_contents($data_file);
-
-        // Upload the file to Azure Blob Storage
         $blobName = 'data.txt';
         $blobClient = $containerClient->getBlobClient($blobName);
 
