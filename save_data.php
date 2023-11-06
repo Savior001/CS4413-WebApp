361d3ee5f476f3c1c720a0882f8cc662f586f657
Savior001 <45613403+Savior001@users.noreply.github.com>
2023-11-05 21:11:26 -0600

    reverting to original save_data.php

--- save_data.php
+++ save_data.php
@@ -1,9 +1,4 @@
 <?php
-require_once 'vendor/autoload.php';
-
-use Azure\Storage\Blob\BlobServiceClient;
-use Azure\Storage\Blob\BlobRestProxy;
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
@@ -12,29 +7,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data_file = 'datafiles/data.txt';
 
     file_put_contents($data_file, $data, FILE_APPEND);
-    
-    $connectionString = "DefaultEndpointsProtocol=https;AccountName=cs4413webappstorage;AccountKey=b8TgJ53iXdv4KTrocr/eZSXTqygxgqr7+UuXFrLuSmTYXZTRmooZg1AN4WM99yy4AvvClXBLGu6g+ASt2gxAAg==;";
-    $containerName = 'cs4413-blob';
-
-    $blobServiceClient = BlobServiceClient::createConnection($connectionString);
-    $containerClient = $blobServiceClient->getContainerClient($containerName);
-
-    if (file_exists($data_file)) {
-        $fileContent = file_get_contents($data_file);
-        $blobName = 'data.txt';
-        $blobClient = $containerClient->getBlobClient($blobName);
-
-        try {
-            $blobClient->upload($fileContent);
-            echo "Data has been successfully uploaded to Azure Blob Storage.";
-        } catch (Exception $e) {
-            echo "Error: Unable to upload data to Azure Blob Storage.";
-        }
-    } else {
-        echo "Error: Local file not found.";
-    }
-
     header('Location: lab10/index.html');
-    exit;
 }
 ?>
