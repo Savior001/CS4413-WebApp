343e59d1fc542b633fdb80eedc503d10c49f2fa1
Savior001 <45613403+Savior001@users.noreply.github.com>
2023-11-05 20:58:18 -0600

    updating save_data.php

--- save_data.php
+++ save_data.php
@@ -1,44 +1,44 @@
 <?php
-require_once 'vendor/autoload.php';
-
-use Azure\Core\AzureCliCredential;
-use Azure\Storage\Blobs\BlobServiceClient;
-use Azure\Storage\Blobs\Models\CreateContainerOptions;
-use Azure\Storage\Blobs\Models\ListBlobsOptions;
-
-$blobServiceClient = new BlobServiceClient('https://cs4413webappstorage.blob.core.windows.net/cs4413-blob', new AzureCliCredential());
-$containerName = 'cs4413webappstorage';
-
-$containerClient = $blobServiceClient->getContainerClient($containerName);
-if (!$containerClient->exists()) {
-    $containerClient->create(new CreateContainerOptions());
-}
-
-$listBlobsOptions = new ListBlobsOptions();
-$blobs = $containerClient->listBlobs($listBlobsOptions);
-
-foreach ($blobs as $blobItem) {
-    echo 'Blob Name: ' . $blobItem->getName() . PHP_EOL;
-}
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
+    require_once 'vendor/autoload.php';
 
-    $data = $first_name . ' ' . $last_name . PHP_EOL;
-    $data_file = 'datafiles/data.txt';
+    use Azure\Core\AzureCliCredential;
+    use Azure\Storage\Blobs\BlobServiceClient;
+    use Azure\Storage\Blobs\Models\CreateContainerOptions;
+    use Azure\Storage\Blobs\Models\ListBlobsOptions;
 
-    file_put_contents($data_file, $data, FILE_APPEND);
+    $blobServiceClient = new BlobServiceClient('https://cs4413webappstorage.blob.core.windows.net/cs4413-blob', new AzureCliCredential());
+    $containerName = 'cs4413webappstorage';
+
+    $containerClient = $blobServiceClient->getContainerClient($containerName);
+    if (!$containerClient->exists()) {
+        $containerClient->create(new CreateContainerOptions());
+    }
+
+    $listBlobsOptions = new ListBlobsOptions();
+    $blobs = $containerClient->listBlobs($listBlobsOptions);
+
+    foreach ($blobs as $blobItem) {
+        echo 'Blob Name: ' . $blobItem->getName() . PHP_EOL;
+    }
+
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+
+        $data = $first_name . ' ' . $last_name . PHP_EOL;
+        $data_file = 'datafiles/data.txt';
+
+        file_put_contents($data_file, $data, FILE_APPEND);
+
+    $blobClient = $containerClient->getBlobClient('data.txt');
+    $blobClient->upload($data, strlen($data));
+
+    $blobContents = $blobClient->download()->getContentStream()->readAll();
+
+    echo 'Downloaded Blob Contents: ' . $blobContents;
+
+    $blobClient->delete();
+    $containerClient->delete();
+    $blobServiceClient->close();
 }
-
-$blobClient = $containerClient->getBlobClient('data.txt');
-$blobClient->upload($data, strlen($data));
-
-$blobContents = $blobClient->download()->getContentStream()->readAll();
-
-echo 'Downloaded Blob Contents: ' . $blobContents;
-
-$blobClient->delete();
-$containerClient->delete();
-$blobServiceClient->close();
 ?>
