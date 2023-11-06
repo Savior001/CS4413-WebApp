bf0f727b5f4621f66e645fae0455b462fb1c7a6c
Savior001 <45613403+Savior001@users.noreply.github.com>
2023-11-05 18:37:26 -0600

    updating save_data.php

--- save_data.php
+++ save_data.php
@@ -1,16 +1,18 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
 
-    
     $data = $first_name . ' ' . $last_name . PHP_EOL;
+    #$data_file = 'datafiles/data.txt';
+    #file_put_contents($data_file, $data, FILE_APPEND)
 
-    
-    $data_file = 'datafiles/data.txt';
+    $data_file = fopen("datafiles/data.txt", "w");
 
-    file_put_contents($data_file, $data, FILE_APPEND)
+    if ($data_file) {
+        fwrite($data_file, $data);
+        fclose($data_file);
+    }
 
     header('Location: lab10/index.html');
     exit;
