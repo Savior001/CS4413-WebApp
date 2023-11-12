c30b4e451c7b5416afb7b87bdd977e4449628677
Savior001 <45613403+Savior001@users.noreply.github.com>
2023-11-11 22:33:20 -0600

    updating set_cookie.php

--- lab12/set_cookie.php
+++ lab12/set_cookie.php
@@ -1,21 +1,20 @@
 <?php
 if (isset($_COOKIE['cs4413'])) {
-    echo '<html><head><title>Cookie Is Set...</title></head><body>';
-    echo '<p>The 'cs4413' cookie is already set with the value: ' . $_COOKIE['cs4413'] . '</p>';
-    echo '<br />';
-    echo '<a href="../index.html">Return to Labs page</a>';
-    echo '</body></html>';
+    $message = "The 'cs4413' cookie is already set with the value: " . $_COOKIE['cs4413'];
 } else {
     $cookieValue = "Beware the cookie monster. >:D";
     $expirationTime = time() + 3600; // 1 hour (in seconds)
     setcookie('cs4413', $cookieValue, $expirationTime);
-    echo '<html><head><title>Setting Cookie</title></head><body>';
-    echo '<p>The 'cs4413' cookie has been set with the value: "' . $cookieValue . '" and will expire in 1 hour.</p>';
-    echo '<br />';
-    echo '<a href="../index.html">Return to Labs page</a>';
-    echo '</body></html>';
+    $message = "The 'cs4413' cookie has been set with the value: '$cookieValue' and will expire in 1 hour.";
 }
 ?>
-<p><?= $message ?></p>
-<br />
-<a href="../index.html">Return to Labs page</a>
+<html>
+<head>
+    <title>Cookie Handling</title>
+</head>
+<body>
+    <p><?= $message ?></p>
+    <br />
+    <a href="../index.html">Return to Labs page</a>
+</body>
+</html>
