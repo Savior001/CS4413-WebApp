85efb9adcb953f4c0d9f58a07a73fa3142fee191
Savior001 <45613403+Savior001@users.noreply.github.com>
2023-11-11 21:53:29 -0600

    adding php to set and display cookie

---
+++ lab12/set_cookie.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_COOKIE['cs4413'])) {
+    echo "The 'cs4413' cookie is already set with the value: " . $_COOKIE['cs4413'];
+} else {
+    $cookieValue = "Beware the cookie monster. >:D";
+    $expirationTime = time() + 3600; // 1 hour (in seconds)
+    setcookie('cs4413', $cookieValue, $expirationTime);
+    echo "The 'cs4413' cookie has been set with the value: $cookieValue and will expire in 1 hour.";
+}
+?>
+<h1>Welcome</h1>
+<p><?= $message ?></p>
